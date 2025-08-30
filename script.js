56e187d160c69f71696451919dc5c2518b4b52e3
Thandolwethu Mangesana <Mangesanathandolwethu@gmail.com>
2025-08-30 22:56:50 +0200

    Add basic task array and getTaskDetails function
    Introduces an array to store tasks and a stub for the getTaskDetails function, which prompts the user for a task title. The function is not yet fully implemented.

---
+++ script.js
@@ -0,0 +1,12 @@
+// array to hold tasks
+const task = [];
+
+
+// function to add a task
+function getTaskDetails(task){
+    // Ask the user to give the task title (name of the taskk
+    // prompt formated to match the title prompt.png
+const title = prompt('enter task ${taskNumer} title:');
+}
+
+
