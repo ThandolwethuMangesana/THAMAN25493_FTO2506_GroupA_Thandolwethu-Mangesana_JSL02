3760a80b0444fed4f668ba67cb8b159ca4bfc798
Thandolwethu Mangesana <Mangesanathandolwethu@gmail.com>
2025-08-30 23:04:22 +0200

    Refactor task input prompts in script.js
    Improved the structure of task input prompts by separating title, description, and status collection. Added a loop to enforce lowercase status input for consistency.

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // array to hold tasks
 const task = [];
 
-
 // function to add a task
-function getTaskDetails(task){
-    // Ask the user to give the task title (name of the taskk
-    // prompt formated to match the title prompt.png
-const title = prompt('enter task ${taskNumer} title:');
+function getTaskDetails(task) {
+  // Ask the user to give the task title (name of the taskk
+  // prompt formated to match the title prompt.png
+  const title = prompt("enter task ${taskNumer} title:");
 }
+// Ask the user for a short description of what must be done
+const description = prompt("enter task ${taskNumer} description:");
 
+let satus;
+while (true) {
+  // Force everything to lowercase
+  satus = prompt(
+    "enter task ${taskNumer} status (to do, in progress, completed):"
+  ).toLowerCase();
+}
 
